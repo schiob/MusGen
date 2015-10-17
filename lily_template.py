49770522700f15b0fe6aa7aa3bf82c20519355c1
schiob <schiob4@gmail.com>
2015-10-16 19:35:00 -0500

    added template and auto .ly creation

--- lily_template.py
+++ lily_template.py
@@ -1,14 +1,11 @@
-global = {
-  \key g \major
+TEMPLATE = r"""global = {
+  \key $key \$grade
   \time 4/4
 }
 
 \parallelMusic #'(voiceA voiceB voiceC voiceD) {
   % Bar 1
-b' a' c'' d'' c'' d'' c'' c'' d'' e'' b' a' a' b' c'' g' c'' b' g' b' d'' fis' g'|
-d' d' e' fis' e' g' e' g' fis' a' g' fis' d' d' e' d' e' d' e' e' fis' d' d'|
-g fis g g a b c' c' d' e' e' a fis g e g g g g g a d g|
-g, d c b, a, g, g e d c e d d g, a, b, c b, c e d d, g,|
+$notes
   % Bar 2 ...
 }
 
@@ -33,4 +30,4 @@ g, d c b, a, g, g e d c e d d g, a, b, c b, c e d d, g,|
   >>
   \layout{}
   \midi{}
-}
+}"""
